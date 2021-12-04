75f6c5b16e7021c814bf8f4d209c0df384878445
Dmitriy Volchenko <dmitriyvolchenko3003@gmail.com>
2021-12-04 17:04:17 +0200

    create build

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
       <Router>
         <AppHeader/>
         <Switch>
-          <Route exact path='/' render={() => {
+          <Route exact path='./' render={() => {
             return (
               <div>
                 <Search/>
